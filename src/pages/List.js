f81de658ea118542e76de8b41dce954161432092
Patrick Abrahim <48718782+AbrahimPatrick@users.noreply.github.com>
2019-10-16 18:56:22 -0300

    Add websocket to project.

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, ScrollView, AsyncStorage, Text, Image } from 'react-native';
+import { StyleSheet, ScrollView, AsyncStorage, Text, Image, Alert } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
+import socketio from 'socket.io-client';
 
 import SpotList from '../components/SpotList';
 
@@ -8,6 +9,20 @@ import logo from '../assets/logo.png';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const allowedOrigins = "//192.168.15.20:19000";
+      const socket = socketio('http://192.168.15.20:3333', {
+        query: {'user_id': user_id},
+        origins: allowedOrigins,
+        transports: ['websocket', 'polling', 'flashsocket']
+      })
+      socket.on('booking_response', booking => {
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+      })
+    })
+  }, [])
 
   useEffect(() => {
     AsyncStorage.getItem('techs').then(storagedTechs => {
